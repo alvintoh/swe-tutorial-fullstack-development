dfc60207d03aa6355a8228b445abfe109f23d5c6
ALVIN-DESKTOP\Alvin Toh <alvin4292@gmail.com>
2026-01-03 12:54:12 +1100

    feat(realtime-slack-clone-project): finished until integration with convex db

--- nextjs/realtime-slack-clone-project/src/features/auth/components/sign-in-card.tsx
+++ nextjs/realtime-slack-clone-project/src/features/auth/components/sign-in-card.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
+import { useState } from "react";
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 
@@ -16,6 +17,9 @@ interface SignInCardProps {
 }
 
 export const SignInCard = ({ setState }: SignInCardProps) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <Card className="w-full h-full p-8">
       <CardHeader className="px-0 pt-0">
@@ -28,16 +32,16 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
         <form className="space-y-2.5">
           <Input
             disabled={false}
-            value=""
-            onChange={() => {}}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
             type="email"
             required
           />
           <Input
             disabled={false}
-            value=""
-            onChange={() => {}}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
             type="password"
             required
@@ -75,7 +79,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
             onClick={() => setState("signUp")}
             className="text-sky-700 hover:underline cursor-pointer"
           >
-            Sign Up
+            Sign up
           </span>
         </p>
       </CardContent>
