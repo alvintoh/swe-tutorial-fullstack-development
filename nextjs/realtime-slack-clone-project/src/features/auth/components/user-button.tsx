3e128027434615d19e619b4c0d17588bb84d336d
ALVIN-DESKTOP\Alvin Toh <alvin4292@gmail.com>
2026-01-09 18:39:26 +1100

    feat(realtime-slack-clone-project): finished until integration with UserButton, AvatarImage and AvatarFallback

---
+++ nextjs/realtime-slack-clone-project/src/features/auth/components/user-button.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { AvatarImage } from "@radix-ui/react-avatar";
+import { DropdownMenuContent } from "@radix-ui/react-dropdown-menu";
+import { Loader } from "lucide-react";
+import { useCurrentUser } from "../api/use-current-user";
+
+export const UserButton = () => {
+  const { data, isLoading } = useCurrentUser();
+
+  if (isLoading) {
+    return <Loader className="size-4 animate-spin text-muted-foreground" />;
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  const { image, name, email } = data;
+
+  const avatarFallback = name!.charAt(0).toUpperCase();
+
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger className="outline-none relative">
+        <Avatar className="size-10 hover:opacity-75 transition">
+          <AvatarImage alt={name} src={image} />
+          <AvatarFallback>{avatarFallback}</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="center" side="right" className="w-60">
+        <DropdownMenuItem className="cursor-default justify-center"></DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
