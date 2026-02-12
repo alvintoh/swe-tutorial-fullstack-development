da71d7b17349aafe6a7db9d5622e1f52b29a4f2d
ALVIN-DESKTOP\Alvin Toh <alvin4292@gmail.com>
2026-02-13 00:00:44 +1100

    test(realtime-slack-project): add bun test and app/page.tsx

---
+++ nextjs/realtime-slack-clone-project/src/app/page.test.tsx
@@ -0,0 +1,9 @@
+import { describe, expect, test } from "bun:test";
+import Home from "./page";
+
+describe("Home Page", () => {
+  test("should export default component", () => {
+    expect(Home).toBeDefined();
+    expect(typeof Home).toBe("function");
+  });
+});
