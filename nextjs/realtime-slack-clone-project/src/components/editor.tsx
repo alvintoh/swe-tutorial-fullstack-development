c7f8092fcd19c26cd06a9048a50ac6b299cc3f14
ALVIN-DESKTOP\Alvin Toh <alvin4292@gmail.com>
2026-02-25 00:02:03 +1100

    feat(realtime-slack-project): finished image upload and input upload

--- nextjs/realtime-slack-clone-project/src/components/editor.tsx
+++ nextjs/realtime-slack-clone-project/src/components/editor.tsx
@@ -1,7 +1,14 @@
 import { Skin } from "@emoji-mart/data";
-import { ImageIcon, Smile } from "lucide-react";
+import { ImageIcon, Smile, XIcon } from "lucide-react";
+import Image from "next/image";
 import Quill, { Delta, Op, QuillOptions } from "quill";
-import { RefObject, useEffect, useLayoutEffect, useRef, useState } from "react";
+import {
+  MutableRefObject,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import { MdSend } from "react-icons/md";
 import { PiTextAa } from "react-icons/pi";
 
@@ -23,7 +30,7 @@ interface EditorProps {
   placeholder?: string;
   defaultValue?: Delta | Op[];
   disabled?: boolean;
-  innerRef?: RefObject<Quill | null>;
+  innerRef?: MutableRefObject<Quill | null>;
   variant?: "create" | "update";
 }
 
@@ -37,6 +44,7 @@ const Editor = ({
   variant = "create",
 }: EditorProps) => {
   const [text, setText] = useState("");
+  const [image, setImage] = useState<File | null>(null);
   const [isToolbarVisible, setIsToolbarVisible] = useState(true);
 
   const containerRef = useRef<HTMLDivElement>(null);
@@ -46,6 +54,7 @@ const Editor = ({
   const quillRef = useRef<Quill | null>(null);
   const defaultValueRef = useRef(defaultValue);
   const disabledRef = useRef(disabled);
+  const imageElementRef = useRef<HTMLInputElement>(null);
 
   useLayoutEffect(() => {
     submitRef.current = onSubmit;
@@ -140,8 +149,40 @@ const Editor = ({
 
   return (
     <div className="flex flex-col">
+      <input
+        type="file"
+        accept="image/*"
+        ref={imageElementRef}
+        onChange={(element) =>
+          setImage(element.target.files ? element.target.files[0] : null)
+        }
+        className="hidden"
+      />
       <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
         <div ref={containerRef} className="h-full ql-custom" />
+        {!!image && (
+          <div className="p-2">
+            <div className="relative size-[62px] flex items-center justify-center group/image">
+              <Hint label="Remove image">
+                <button
+                  onClick={() => {
+                    setImage(null);
+                    imageElementRef.current!.value = "";
+                  }}
+                  className="hidden group-hover/image:flex rounded-full bg-black/70 absolute -top-2.5 -right-2.5 text-white size-6 z-[4] border-2 border-white items-center justify-center"
+                >
+                  <XIcon className="size-3.5" />
+                </button>
+              </Hint>
+              <Image
+                src={URL.createObjectURL(image)}
+                alt="uploaded"
+                fill
+                className="rounded-xl overflow-hidden border object-cover"
+              />
+            </div>
+          </div>
+        )}
         <div className="flex px-2 pb-2 z-5">
           <Hint
             label={isToolbarVisible ? "Hide formatting" : "Show formatting"}
@@ -157,12 +198,7 @@ const Editor = ({
           </Hint>
           <Hint label="Emoji">
             <EmojiPopover onEmojiSelect={onEmojiSelect}>
-              <Button
-                disabled={disabled}
-                size="icon-sm"
-                variant="ghost"
-                onClick={() => {}}
-              >
+              <Button disabled={disabled} size="icon-sm" variant="ghost">
                 <Smile className="size-4" />
               </Button>
             </EmojiPopover>
@@ -172,7 +208,7 @@ const Editor = ({
               disabled={disabled}
               size="icon-sm"
               variant="ghost"
-              onClick={() => {}}
+              onClick={() => imageElementRef.current?.click()}
             >
               <ImageIcon className="size-4" />
             </Button>
